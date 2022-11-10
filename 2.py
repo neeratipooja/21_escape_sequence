1ecfcd428a28e79b3c616114e3fb90110dfafc30
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-11-10 18:08:08 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,17 @@
+'''
+DATE=10TH NOV 2022
+DAY= THURSDAY
+TOPIC= ESCAPE SEQUENCES 
+AUTHOR= KIRANMAI
+'''
+print("MY NAME IS \nKIRANMAI") #MY NAME IS 
+                               # KIRANMAI
+print("kiranmai\akothaoelli") # kiranmaikothapelli 
+print("enjoying movie\\atoday") #enjoying moving\atoday
+print("enjoying movie \btoday")#enjoying movietoday
+print("enjoying movie\btoday")#enjoying movitoday
+print("enjoying movie\ttoday")#enjoying movie today
+print("enjoying movie\ftoday")#enjoying movie
+                              #today
+print("enjoying movie\rtoday")#todaying movie
+print("enjoyingmovieto\rday")#dayoyingmovieto
